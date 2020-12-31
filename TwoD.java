644c1089fd2016197f08fc3cd2af6a9b957bc9ba
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-12-31 22:25:15 +0530

    Add files via upload
    updated

---
+++ TwoD.java
@@ -0,0 +1,27 @@
+
+package shivaafoundation.javatutorial;
+//Two dimensional coordinates
+class TwoD {
+    int x, y;
+    TwoD(int a, int b){
+        x=a;
+        y=b;
+    }
+    }
+   //Three dimentional coordinates
+class ThreeD extends TwoD {
+    int z;
+    ThreeD(int a, int b, int c){
+        super(a, b);
+        z = c;
+        
+    }
+}
+//Four dimensional coordinates
+class FourD extends ThreeD{
+    int t;
+    FourD(int a, int b, int c,int d){
+        super(a,b,c);
+        t=d;
+    }
+}
