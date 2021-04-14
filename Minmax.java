9ef0955ac1e004c172e1e87ba7f2ee9b00986dff
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2021-04-14 16:13:10 +0530

    Add files via upload
    generic interface

--- Minmax.java
+++ Minmax.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package shivaafoundation.javatutorial;
 
-/**
- *
- * @author SAPNA
- */
-public interface Minmax {
-    
-}
+interface Minmax <T extends Comparable<T>> {
+    T min();
+    T max();
+    }
+ class MyClass<T extends Comparable<T>> implements MinMax<T> {
+     T[] vals;
+     MyClass(T[] o){ vals = o;}
+     public T min(){
+         T v = vals[0];
+        for(int i=1; i < vals.length; i++)
+            if(vals[i].compareTo(v)< o) v = vals[i];
+        return v;
+     }
+     public T max(){
+         T v = vals[0];
+for(int i=1; i < val.length; i++ )
+    if(vals[i].compareTo(v) > 0) v=vals[i];
+return v;
+     }
+ }
+     class GenIFDemo{
+         public static void main(String args[]) {
+             Integer inums[]={3,6,2,8,6};
+             Character chs[]={'b','r','p','w'};
+             MyClass<Integer> iob = new MyClass<Integer>(inums);
+             MyClass<Character> cob = new MyClass<Character>(chs);
+             
+             System.out.println("Max value in chs:" + cob.max());
+             System.out.println("Min value in chs: " + cob.min());
+             
+         }
+         }
+     
